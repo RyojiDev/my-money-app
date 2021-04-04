04d51743e3d333ed6654cfea1a39843ef8532df3
Ryojidev <ryojikitanodev@gmail.com>
2021-04-04 16:40:47 -0300

    adicionado cors.js para permitir requisição de outras origens e modificado server para contemplar o middleware do cors.js

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -1,13 +1,13 @@
 const port = 3003;
 
 const bodyParser = require('body-parser');
-
-const express = require('express')
-
-const server = express()
+const express = require('express');
+const server = express();
+const allowCors = require('./cors');
 
 server.use(bodyParser.urlencoded({ extends : true}))
 server.use(bodyParser.json())
+server.use(allowCors);
 
 server.listen(port, function(){
     console.log(`backend está rodando na porta ${port}`)
