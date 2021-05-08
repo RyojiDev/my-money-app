e8ed65727e5b3e965cfd0fa7de003b3f4467763c
Ryojidev <ryojikitanodev@gmail.com>
2021-05-08 20:47:34 -0300

    implemented style template admin lte in billingCycleForm

--- frontend/src/billingCycle/billingCycleActions.js
+++ frontend/src/billingCycle/billingCycleActions.js
@@ -1,5 +1,10 @@
 import axios from 'axios';
+import { toastr } from 'react-redux-toastr'
+import { reset as resetForm } from 'redux-form'
+import { showTabs, selectTab } from '../common/tab/tabActions'
+
 const BASE_URL = 'http://localhost:3003/api'
+
 
 export function getList(){
   const request = axios.get(`${BASE_URL}/billingCycles`)
@@ -10,8 +15,21 @@ export function getList(){
 }
 
 export function create(values) {
-  axios.post(`${BASE_URL}/billingCycles`, values)
-  return {
-    type: 'TEMP'
+  return dispatch => {
+    axios.post(`${BASE_URL}/billingCycles`, values)
+    .then(resp => {
+      toastr.success('Operação realizada com sucesso')
+      dispatch([
+        resetForm('billingCycleForm'),
+        getList(),
+        selectTab('tabList'),
+        showTabs('tabList', 'tabCreate')
+      ])  
+    })
+    .catch(e => {
+      e.response.data.errors.forEach(error => toastr.error('Erro', error))
+        
+      })
   }
+ 
 }
